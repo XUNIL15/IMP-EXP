af7e57e93a0f751000427fc7810804bed24f3823
XUNIL15 <abdoulmaiga1443@gmail.com>
2026-03-29 20:33:29 +0200

    Delete lib directory

--- lib/db/src/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./clients";
-export * from "./transitaires";
-export * from "./arrivages";
-export * from "./colis";
-export * from "./colis_proprietaires";
-export * from "./paiements";
